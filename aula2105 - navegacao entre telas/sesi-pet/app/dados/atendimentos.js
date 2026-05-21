e0e841bd7d185a129b8934c1356b45f39b0dd0ca
davizoladosmoveisplanejados <davi.sousa17@aluno.senai.br>
2026-05-21 11:03:02 -0300

    Navegação entre Telas

---
+++ aula2105 - navegacao entre telas/sesi-pet/app/dados/atendimentos.js
@@ -0,0 +1,11 @@
+export const profissionais = [
+  { id: '1', nome: 'Dra. Evelin Camila', especialidade: 'Banho e tosa' },
+  { id: '2', nome: 'Dr. Rafael Lima', especialidade: 'Espermograma' },
+  { id: '3', nome: 'Ângelo Tripa', especialidade: 'Mumificação' },
+];
+
+export const atendimentos = [
+  { id: '1', pet: 'Rai Roque', servico: 'Consulta', horario: '09:00' },
+  { id: '2', pet: 'Alemão', servico: 'Banho', horario: '10:30' },
+  { id: '3', pet: 'Regildo', servico: 'Tosa', horario: '14:00' },
+];
